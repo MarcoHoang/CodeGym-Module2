af781d69606488cba9ac68cac19bbb9511c8193b
MarcoHoang <hoang111uuu@gmail.com>
2025-05-07 15:21:11 +0700

    TemplateMethod

---
+++ Behavioral-Design-Pattern/Parctice23/Template-Method/src/main/TemplateMethodDemo.java
@@ -0,0 +1,17 @@
+package main;
+
+import concrete.HamburgerMeal;
+import concrete.TacoMeal;
+import template.Meal;
+
+public class TemplateMethodDemo {
+    public static void main(String[] args) {
+        Meal meal1 = new HamburgerMeal();
+        meal1.doMeal();
+
+        System.out.println();
+
+        Meal meal2 = new TacoMeal();
+        meal2.doMeal();
+    }
+}
