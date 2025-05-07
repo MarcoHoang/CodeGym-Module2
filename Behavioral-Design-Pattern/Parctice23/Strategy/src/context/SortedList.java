208b2d8351603bddd0d839e3cc5c56e7a8b99029
MarcoHoang <hoang111uuu@gmail.com>
2025-05-07 15:13:34 +0700

    Strategy

---
+++ Behavioral-Design-Pattern/Parctice23/Strategy/src/context/SortedList.java
@@ -0,0 +1,23 @@
+package context;
+
+import strategy.SortStrategy;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortedList {
+
+    private SortStrategy strategy;
+    private final List<String> items = new ArrayList<>();
+
+    public void setSortStrategy(SortStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void add(String name) {
+        items.add(name);
+    }
+
+    public void sort() {
+        strategy.sort(items);
+    }
+}
